ea9e9227fe6b7d4ec63745374650dfdabba658c6
Carlos <corteg20@gmail.com>
2017-08-13 23:56:23 -0500

    retabbed

--- popup.js
+++ popup.js
@@ -31,7 +31,7 @@ chrome.tabCapture.capture({audio: true}, function(cap){
 
   var destination = audioContext.destination;
   //TODO: For some reason this stopped working need to look into why
-	//destination.maxChannelCount = 2;
+  //destination.maxChannelCount = 2;
 
   var analyser = audioContext.createAnalyser();
   var analyser2 = audioContext.createAnalyser();
@@ -39,9 +39,9 @@ chrome.tabCapture.capture({audio: true}, function(cap){
   source.connect(analyser);
   source.connect(analyser2);
 
-	//only need one to work
-	analyser.connect(destination);
-	visualize(analyser, analyser2, canvasCtx)
+  //only need one to work
+  analyser.connect(destination);
+  visualize(analyser, analyser2, canvasCtx)
 });
 
 function visualize(analyser, analyser2) {
@@ -49,19 +49,19 @@ function visualize(analyser, analyser2) {
   var bufferLength = analyser.frequencyBinCount;
   var dataArray = new Uint8Array(bufferLength);
 
-	analyser2.fftSize = 256
+  analyser2.fftSize = 256
   var bufferLength2 = analyser2.frequencyBinCount;
   var dataArray2 = new Uint8Array(bufferLength2);
 
   var initOsciCount = 0
 
-	function draw(){
-		canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
-		drawVisual = requestAnimationFrame(draw);
+  function draw(){
+    canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+    drawVisual = requestAnimationFrame(draw);
 
     drawOsci();
     drawCircle();
-	};
+  };
 
   function drawOsci(){
     analyser.getByteTimeDomainData(dataArray);
@@ -98,57 +98,57 @@ function visualize(analyser, analyser2) {
 
   function osciDraw(values, length, yLoc, alphaLevel){
     canvasCtx.lineWidth = 1;
-		canvasCtx.strokeStyle = 'rgba(255, 255, 255,' + alphaLevel  +')';
+    canvasCtx.strokeStyle = 'rgba(255, 255, 255,' + alphaLevel  +')';
 
-		canvasCtx.beginPath();
+    canvasCtx.beginPath();
 
-		var sliceWidth = WIDTH * 1.0 / length;
-		var x = 0;
+    var sliceWidth = WIDTH * 1.0 / length;
+    var x = 0;
 
-		for(var i = 0; i < length; i++) {
-			var v = values[i]/4 - 32;
-			var y = v + yLoc;
+    for(var i = 0; i < length; i++) {
+      var v = values[i]/4 - 32;
+      var y = v + yLoc;
 
-			if(i === 0) {
-				canvasCtx.moveTo(x, y);
-			}
-			else {
-				canvasCtx.lineTo(x, y);
-			}
+      if(i === 0) {
+        canvasCtx.moveTo(x, y);
+      }
+      else {
+        canvasCtx.lineTo(x, y);
+      }
 
-			x += sliceWidth
-		}
+      x += sliceWidth
+    }
 
-		canvasCtx.stroke();
+    canvasCtx.stroke();
   }
 
   function drawCircle(){
-		analyser2.getByteFrequencyData(dataArray2);
+    analyser2.getByteFrequencyData(dataArray2);
 
-		var angleOffSet1 = RADSPERFRAME1 * frameCounter1 + angleDelta1;
-		var angleOffSet2 = (2* Math.PI) - RADSPERFRAME2 * frameCounter2 - angleDelta2;
-		var angleOffSet3 = RADSPERFRAME3 * frameCounter3 + angleDelta3;
+    var angleOffSet1 = RADSPERFRAME1 * frameCounter1 + angleDelta1;
+    var angleOffSet2 = (2* Math.PI) - RADSPERFRAME2 * frameCounter2 - angleDelta2;
+    var angleOffSet3 = RADSPERFRAME3 * frameCounter3 + angleDelta3;
 
-		if(angleOffSet1 > 2 * Math.PI){
-			angleDelta1 = angleOffSet1 - 2 * Math.PI;
-			angleOffSet1 = angleDelta1
-			frameCounter1 = 0
-		}
-		if(angleOffSet2 < 0){
-			angleDelta2 = -1 * angleOffSet2;
-			angleOffSet2 = angleDelta2
+    if(angleOffSet1 > 2 * Math.PI){
+      angleDelta1 = angleOffSet1 - 2 * Math.PI;
+      angleOffSet1 = angleDelta1
+      frameCounter1 = 0
+    }
+    if(angleOffSet2 < 0){
+      angleDelta2 = -1 * angleOffSet2;
+      angleOffSet2 = angleDelta2
 
-			frameCounter2 = 0
-		}
-		if(angleOffSet3 > 2 * Math.PI){
-			angleDelta3 = angleOffSet3 - 2 * Math.PI;
-			angleOffSet3 = angleDelta3
-			frameCounter3 = 0
-		}
+      frameCounter2 = 0
+    }
+    if(angleOffSet3 > 2 * Math.PI){
+      angleDelta3 = angleOffSet3 - 2 * Math.PI;
+      angleOffSet3 = angleDelta3
+      frameCounter3 = 0
+    }
 
-		canvasCtx.lineWidth = 5;
+    canvasCtx.lineWidth = 5;
 
-		canvasCtx.strokeStyle = 'rgba(255, 100, 100, 0.75)';
+    canvasCtx.strokeStyle = 'rgba(255, 100, 100, 0.75)';
     circularDraw(canvasCtx, WIDTH/2, HEIGHT/2, dataArray2, 0, Math.floor(bufferLength2/3), angleOffSet1)
 
     canvasCtx.strokeStyle = 'rgba(0, 250, 250, 0.75)';
@@ -157,10 +157,10 @@ function visualize(analyser, analyser2) {
     canvasCtx.strokeStyle = 'rgba(255, 255, 100, 0.75)';
     circularDraw(canvasCtx, WIDTH/2, HEIGHT/2, dataArray2, Math.floor(bufferLength2 - (bufferLength2/3) + 1), bufferLength2, angleOffSet3);
 
-		frameCounter1++;
-		frameCounter2++;
-		frameCounter3++;
-	}
+    frameCounter1++;
+    frameCounter2++;
+    frameCounter3++;
+  }
 
   draw();
 }
@@ -170,26 +170,26 @@ function circularDraw(ctx, midX, midY, values, lo, hi, startAngle){
   var y;
 
   var bufLength = hi - lo;
-	var numLoops = 4;
+  var numLoops = 4;
   var delta = 2 * Math.PI / (bufLength * numLoops);
 
-	ctx.beginPath();
-	ctx.moveTo(midX, midY);
+  ctx.beginPath();
+  ctx.moveTo(midX, midY);
 
 
-	var goingForward = true
-	var loopCount = 0
-	var lineCount = 0
+  var goingForward = true
+  var loopCount = 0
+  var lineCount = 0
   for(var i = lo - 1; ; ) {
-		if(loopCount == numLoops && i == lo - 1)
-			break;
+    if(loopCount == numLoops && i == lo - 1)
+      break;
 
-		if(goingForward)
-			i++;
-		else
-			i--;
+    if(goingForward)
+      i++;
+    else
+      i--;
 
-		var hypot = values[i];
+    var hypot = values[i];
 
     var angle = lineCount * delta + startAngle;
     if (angle > 2 * Math.PI)
@@ -208,22 +208,22 @@ function circularDraw(ctx, midX, midY, values, lo, hi, startAngle){
       y = midY + b;
 
     ctx.lineTo(x, y);
-		ctx.lineTo(midX, midY);
+    ctx.lineTo(midX, midY);
 
-		if(goingForward && i == hi - 1){
-			loopCount++;
-			goingForward = false;
-			i++;
-		}
-		else if(!goingForward && i == lo){
-			loopCount++;
-			goingForward = true;
-			i--;
-		}
-		lineCount++
-	}
+    if(goingForward && i == hi - 1){
+      loopCount++;
+      goingForward = false;
+      i++;
+    }
+    else if(!goingForward && i == lo){
+      loopCount++;
+      goingForward = true;
+      i--;
+    }
+    lineCount++
+  }
 
-	ctx.lineTo(midX, midY);
-	ctx.stroke();
+  ctx.lineTo(midX, midY);
+  ctx.stroke();
 }
 
